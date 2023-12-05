e71e807173005766439cffd203fe89405764fdb8
s4muraii <viniciusoliveiramoura24@gmail.com>
2023-12-05 09:15:38 -0300

    update

--- src/components/Tabela.jsx
+++ src/components/Tabela.jsx
@@ -1,20 +1,24 @@
 import Table from 'react-bootstrap/Table';
+import Button from 'react-bootstrap/esm/Button';
 import axios from 'axios';
 import { useState } from 'react';
 
-
 function Tabela() {
 
+    const [btnValue, setBtnValue] = useState('Atualizar')
     const [dados, setDados] = useState([])
 
-    axios.get("https://apiaulas.thiagodev502.repl.co/funcionarios").then((resposta) => {
-        console.log(resposta.data)
-    }).catch((erro) => {
-        console.log(erro)
-    })
+    useEffect (() => {axios.get("https://apiaulas.thiagodev502.repl.co/funcionarios").then((resposta) => {
+            setDados(resposta.data)
+        }).catch((erro) => {
+            console.log(erro)
+        })
+    }, [])
+
 
     return (
         <>
+        <button onClick ={()=> setBtnValue}>{btnValue}</button>
             <Table>
                 <thead>
                     <tr>
@@ -30,13 +34,16 @@ function Tabela() {
                             <td>{item.id}</td>
                             <td>{item.nome}</td>
                             <td>{item.cargo}</td>
-                            <td>{item.acao}</td>
+                            <td>
+                                <Button className="btn btn-primary me-2">Editar</Button>
+                                <Button className="btn btn-danger">Excluir</Button>
+                            </td>
                         </tr>
                     ))}
                 </tbody>
             </Table>
         </>
-    );
+    )
 }
 
 export default Tabela
