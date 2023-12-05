0be42765d12114e9004d8bcf28bbafaa352b48bb
s4muraii <viniciusoliveiramoura24@gmail.com>
2023-12-05 08:40:13 -0300

    update

--- src/Pages/Painel.jsx
+++ src/Pages/Painel.jsx
@@ -8,7 +8,7 @@ function Painel() {
         <Header btn={true} />
         <Conteudo>
             <h2>Lista de Funcionarios</h2>
-            <tabela/>
+            <Tabela/>
         </Conteudo>
         </> 
     );
